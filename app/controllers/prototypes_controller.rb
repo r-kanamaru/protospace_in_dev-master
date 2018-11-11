ac34be54565ee596c2b18e20939dd927329505e6
西田昂平 <zelbondtech@gmail.com>
2018-11-11 13:41:41 +0900

    update

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,17 +16,17 @@ class PrototypesController < ApplicationController
       redirect_to :root, notice: 'New prototype was successfully created'
     else
       redirect_to new_prototype_path, alert: 'YNew prototype was unsuccessfully created'
-     end
+    end
   end
 
   def edit
     set_prototype
-    @prototype.captured_images
+    @prototype.captured_images.build
   end
 
   def update
     set_prototype
-    @prototype.update(edit_prototype_params)
+    @prototype.update(prototype_params)
     redirect_to prototype_path
   end
 
@@ -46,16 +46,6 @@ class PrototypesController < ApplicationController
   end
 
   def prototype_params
-    params.require(:prototype).permit(
-      :title,
-      :catch_copy,
-      :concept,
-      :user_id,
-      captured_images_attributes: [:content, :status]
-    )
-  end
-
-  def edit_prototype_params
     params.require(:prototype).permit(
       :title,
       :catch_copy,
