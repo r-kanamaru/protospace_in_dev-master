599b2781bd88751d389547225678aa8e56030f22
西田昂平 <zelbondtech@gmail.com>
2018-11-11 11:24:32 +0900

    update resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :edit, :show, :update, :delete]
+  resources :prototypes, only: [:index, :new, :create, :edit, :show, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
